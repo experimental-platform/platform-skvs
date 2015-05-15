69967f60c22f98dd783595bd81a71c6bde9a576b
Andreas Litt <andreas.litt@gmail.com>
2015-05-15 11:20:10 +0200

    download only in ci

--- ci-build.sh
+++ ci-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # THIS ONLY WORK IN OUR CI!
-docker run --rm -v /data/jenkins/jobs/docker-skvs-build/workspace:/usr/src/skvs -w /usr/src/skvs golang:1.4 /bin/bash -c 'go get && go build -v'
+docker run --rm -v /data/jenkins/jobs/docker-skvs-build/workspace:/usr/src/skvs -w /usr/src/skvs golang:1.4 /bin/bash -c 'go get -d && go build -v'
