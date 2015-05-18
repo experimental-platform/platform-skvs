ab3a85bbce4f6123f442eb577468c80d43fc69e3
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-05-18 15:22:25 +0200

    Fixed relative pathname.

--- ci-build.sh
+++ ci-build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # THIS ONLY WORK IN OUR CI!
 
-echo "XXX: Jenkins variable:  " ${JOB_NAME}
-echo "XXX: required path: /data/jenkins/jobs/docker-skvs-build/workspace"
-
-docker run --rm -v /data/jenkins/jobs/docker-skvs-build/workspace:/usr/src/skvs -w /usr/src/skvs golang:1.4 /bin/bash -c 'go get -d && go build -v'
+docker run --rm -v /data/jenkins/jobs/${JOB_NAME}/workspace:/usr/src/skvs -w /usr/src/skvs golang:1.4 /bin/bash -c 'go get -d && go build -v'
