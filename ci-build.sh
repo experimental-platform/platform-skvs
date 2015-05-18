e10c6c736e12d6e4cd9d9bebcc624b4baa767c41
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-05-18 15:17:15 +0200

    DEBUG PWD.

--- ci-build.sh
+++ ci-build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # THIS ONLY WORK IN OUR CI!
+
+echo "XXX: current path:  " $(pwd)
+echo "XXX: required path: /data/jenkins/jobs/docker-skvs-build/workspace"
+
 docker run --rm -v /data/jenkins/jobs/docker-skvs-build/workspace:/usr/src/skvs -w /usr/src/skvs golang:1.4 /bin/bash -c 'go get -d && go build -v'
