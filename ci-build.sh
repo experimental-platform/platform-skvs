ff1a7a5bca5fbf2dedaca5989cb6fa5ad0c5b3ba
Andreas Litt <andreas.litt@gmail.com>
2015-06-11 17:12:49 +0200

    run tests in ci-build

--- ci-build.sh
+++ ci-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # THIS ONLY WORK IN OUR CI!
 
-docker run --rm -v /data/jenkins/jobs/${JOB_NAME}/workspace:/usr/src/skvs -w /usr/src/skvs golang:1.4 /bin/bash -c 'go get -d && go build -v'
+docker run --rm -v /data/jenkins/jobs/${JOB_NAME}/workspace:/usr/src/skvs -w /usr/src/skvs golang:1.4 /bin/bash -c 'go get -d && go test -v && go build -v'
