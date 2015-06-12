79c5f2e2a2f906812851a48c88319c39cf731f05
Andreas Litt <andreas.litt@gmail.com>
2015-06-12 12:24:10 +0200

    add more tests

--- server_test.go
+++ server_test.go
@@ -16,6 +16,7 @@ func TestExpandPath(t *testing.T) {
 }
 
 func TestPutKeyWithoutNamespace(t *testing.T) {
+	cleanData()
 	testPath := expandPath("foobar")
 	testContent := "foobar"
 	err := putKey(testPath, testContent)
@@ -28,6 +29,7 @@ func TestPutKeyWithoutNamespace(t *testing.T) {
 }
 
 func TestPutKeyWithNamespace(t *testing.T) {
+	cleanData()
 	testPathDirectory := expandPath("foo/")
 	testPathFile := expandPath("foo/bar")
 	testContent := "foobar"
@@ -44,6 +46,7 @@ func TestPutKeyWithNamespace(t *testing.T) {
 }
 
 func TestDeleteKey(t *testing.T) {
+	cleanData()
 	path := expandPath("foobar")
 	content := "foobar"
 	if err := os.MkdirAll(filepath.Dir(path), os.ModePerm); err != nil {
@@ -58,9 +61,64 @@ func TestDeleteKey(t *testing.T) {
 	}
 }
 
+func TestReadKeyWithoutNamespace(t *testing.T) {
+	cleanData()
+	testPath := expandPath("foobar")
+	testContent := "foobar"
+	if err := os.MkdirAll(filepath.Dir(testPath), os.ModePerm); err != nil {
+		t.Errorf("Could not create key '%s'\n", testPath)
+	}
+	if err := ioutil.WriteFile(testPath, []byte(testContent), os.ModePerm); err != nil {
+		t.Errorf("Could not write file '%s' with content '%s'\n", testPath, testContent)
+	}
+
+	results, isNamespace, err := readKey(testPath)
+	if err != nil {
+		t.Error(err)
+	}
+	if isNamespace {
+		t.Error("Is namespaced value, but should not be.")
+	}
+	if len(results) != 1 || results[0] != testContent {
+		t.Errorf("Too many results given (%+v) or first result has not expected content (%s).", results, testContent)
+	}
+}
+
+func TestReadKeyWithNamespace(t *testing.T) {
+	cleanData()
+	testPathDirectory := expandPath("foo/")
+	testPathFile1 := expandPath("foo/bar1")
+	testPathFile2 := expandPath("foo/bar2")
+	testContent := "foobar"
+	if err := os.MkdirAll(testPathDirectory, os.ModePerm); err != nil {
+		t.Errorf("Could not create directory '%s'\n", testPathDirectory)
+	}
+	if err := ioutil.WriteFile(testPathFile1, []byte(testContent), os.ModePerm); err != nil {
+		t.Errorf("Could not write file '%s' with content '%s'\n", testPathFile1, testContent)
+	}
+	if err := ioutil.WriteFile(testPathFile2, []byte(testContent), os.ModePerm); err != nil {
+		t.Errorf("Could not write file '%s' with content '%s'\n", testPathFile2, testContent)
+	}
+
+	results, isNamespace, err := readKey(testPathDirectory)
+	if err != nil {
+		t.Error(err)
+	}
+	if !isNamespace {
+		t.Error("Is not namespaced value, but should be.")
+	}
+	if len(results) != 2 {
+		t.Errorf("Too many/few results given (%+v).", results)
+	}
+}
+
+func cleanData() {
+	os.RemoveAll(opts.DataPath)
+}
+
 func TestMain(m *testing.M) {
 	opts.DataPath, _ = filepath.Abs("./data-test")
 	exit := m.Run()
-	os.RemoveAll(opts.DataPath)
+	cleanData()
 	os.Exit(exit)
 }
