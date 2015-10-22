9744122e911d51c7f249d61959fbbf60399b48ad
Kamil Domański <kamil@domanski.co>
2015-10-23 00:09:47 +0200

    fix CI build

--- ci-build.sh
+++ ci-build.sh
@@ -3,4 +3,4 @@ set -e
 
 SRC_PATH=$(pwd)
 
-docker run -v ${SRC_PATH}:/usr/src/skvs -w /usr/src/skvs golang:1.4 /bin/bash -c 'go get -d && go test -v && go build -v'
+docker run -v ${SRC_PATH}:/usr/src/platform-skvs -w /usr/src/platform-skvs golang:1.4 /bin/bash -c 'go get -d && go test -v && go build -v'
