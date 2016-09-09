525cfa3f0794d0b2b91edf54f2248b4f7f7ec230
Hinnerk Haardt <haardt@information-control.de>
2016-09-09 11:35:28 +0200

    Function moved to the utility library.

--- client/main.go
+++ client/main.go
@@ -2,16 +2,11 @@ package client
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-
-	"github.com/docker/engine-api/client"
-	"github.com/docker/engine-api/types"
-	"github.com/docker/engine-api/types/filters"
-	"golang.org/x/net/context"
+	"github.com/experimental-platform/platform-utils/dockerutil"
 )
 
 type skvsResponse struct {
@@ -20,41 +15,10 @@ type skvsResponse struct {
 	Value     string `json:"value"`
 }
 
-func GetContainerIP(name string) (string, error) {
-	defaultHeaders := map[string]string{"User-Agent": "protonet-skvs_cli"}
-	cli, err := client.NewClient("unix:///var/run/docker.sock", "v1.22", nil, defaultHeaders)
-	if err != nil {
-		return "", err
-	}
-
-	listOptions := types.ContainerListOptions{Filter: filters.NewArgs()}
-	listOptions.Filter.Add("name", name)
-
-	containers, err := cli.ContainerList(context.Background(), listOptions)
-	if err != nil {
-		return "", err
-	}
-	if len(containers) == 0 {
-		return "", fmt.Errorf("Found no container named '%s'", name)
-	}
-
-	data, err := cli.ContainerInspect(context.Background(), containers[0].ID)
-	if err != nil {
-		return "", err
-	}
-
-	protonetNetworkData, ok := data.NetworkSettings.Networks["protonet"]
-	if !ok {
-		return "", errors.New("The SKVS container doesn't belong to the network 'protonet'.")
-	}
-
-	return protonetNetworkData.IPAddress, nil
-}
-
 // Get retrieves a value of an SKVS key
 // It does not propely handle namespaces
 func Get(key string) (string, error) {
-	ip, err := GetContainerIP("skvs")
+	ip, err := dockerutil.GetContainerIP("skvs")
 	if err != nil {
 		return "", err
 	}
@@ -87,7 +51,7 @@ func Get(key string) (string, error) {
 
 // Set sets a value of a given SKVS key
 func Set(key string, value string) error {
-	ip, err := GetContainerIP("skvs")
+	ip, err := dockerutil.GetContainerIP("skvs")
 	if err != nil {
 		return err
 	}
@@ -110,7 +74,7 @@ func Set(key string, value string) error {
 
 // Delete removes an SKVS entry and all its children
 func Delete(key string) error {
-	ip, err := GetContainerIP("skvs")
+	ip, err := dockerutil.GetContainerIP("skvs")
 	if err != nil {
 		return err
 	}
